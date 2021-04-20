d1ba5e099098b94fdc26a8ee32cd9dfb39cae939
ahmednr-dev <ahmednr.dev@gmail.com>
2021-04-21 00:55:41 +0200

    Add PersonData

---
+++ Person/Person/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace Person
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.showBtn = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.96353F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.11246F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.92401F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 329);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(241, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Person Data";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.Controls.Add(this.txtAddress, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtName, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 95);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(652, 149);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(198, 101);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(451, 43);
+            this.txtAddress.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.txtAddress, "Enter Person Address");
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(198, 52);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(451, 43);
+            this.txtName.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.txtName, "Enter Person Name");
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(37, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 38);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Address";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(74, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(50, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 38);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(198, 3);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(451, 43);
+            this.txtID.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txtID, "Enter Person ID");
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.Controls.Add(this.btnExit, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.showBtn, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnFind, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 250);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(652, 76);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Blue;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(494, 5);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 66);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // showBtn
+            // 
+            this.showBtn.BackColor = System.Drawing.Color.Blue;
+            this.showBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.showBtn.ForeColor = System.Drawing.Color.White;
+            this.showBtn.Location = new System.Drawing.Point(331, 5);
+            this.showBtn.Margin = new System.Windows.Forms.Padding(5);
+            this.showBtn.Name = "showBtn";
+            this.showBtn.Size = new System.Drawing.Size(153, 66);
+            this.showBtn.TabIndex = 2;
+            this.showBtn.Text = "Show All";
+            this.showBtn.UseVisualStyleBackColor = false;
+            this.showBtn.Click += new System.EventHandler(this.showBtn_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.Color.Blue;
+            this.btnFind.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFind.ForeColor = System.Drawing.Color.White;
+            this.btnFind.Location = new System.Drawing.Point(168, 5);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(153, 66);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Blue;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(5, 5);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(153, 66);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Notes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(682, 353);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Person";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button showBtn;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Person/Person/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.IO; // File Heandling 
+using System.Windows.Forms;
+
+namespace Person
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add icon From progrem to avoid size increse 
+           
+            // method 1 [ meet problem if change program name ]
+            //this.Icon = Icon.ExtractAssociatedIcon("Person.exe");
+            // Method 2 
+            // note icon dosen't appear in Visual studio open from folder of bin/debug
+            this.Icon = Icon.ExtractAssociatedIcon(AppDomain.CurrentDomain.FriendlyName + ".exe");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+   {
+    try
+            {
+    // Validate 
+     // trim to delete white speace from txtbox
+     if (txtID.Text.Trim() == "" || txtName.Text.Trim() == "" || txtAddress.Text.Trim() == "")
+         {
+             MessageBox.Show("Please enter all data");
+             return;
+         }
+
+         // Read From File 
+         StreamReader fCheck = new StreamReader("Data.txt");
+         string strCheck = fCheck.ReadToEnd();
+         fCheck.Close();
+
+         if (strCheck.Contains(txtID.Text + ";"))
+         {
+             MessageBox.Show("ID already Exits , try another one ");
+             txtID.Focus();
+             txtID.SelectAll();
+
+         }
+         else
+         {
+             // Write to file 
+             StreamWriter sw = new StreamWriter("Data.txt", true);  // true will not override file,Append to file 
+
+             string strPerson = txtID.Text + ";"
+                                + txtName.Text + ";"
+                                + txtAddress.Text;
+             sw.WriteLine(strPerson);
+             sw.Close();
+             MessageBox.Show("Person is Added");
+
+                    // clear forms after added
+                    /* foreach (Control c in this.Controls)
+                     {  
+                         if (c is TextBox)
+                             c.Text = " ";
+                     }*/
+                    txtID.Text = "";
+             txtName.Text = "";
+             txtAddress.Text = "";
+             txtID.Focus();
+         }
+     }
+     catch(Exception err)
+     {MessageBox.Show(err.Message);}
+           
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtID.Text.Trim() != "")
+                {
+                    StreamReader sr = new StreamReader("Data.txt");
+                    string line = " ";
+                    bool found = false;
+                    do
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            string[] array = line.Split(';');
+                            if (txtID.Text == array[0])
+                            {
+                                txtName.Text = array[1];
+                                txtAddress.Text = array[2];
+                                found = true;
+                                break;
+                            }
+                        }
+                    } while (line != null);
+                    sr.Close();
+                    if (!found)
+                    {
+                        MessageBox.Show("ID not found");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter Valid ID");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void showBtn_Click(object sender, EventArgs e)
+        {
+            Form fmShow = new Form();
+            TextBox txtShow = new TextBox(); 
+            fmShow.StartPosition = FormStartPosition.CenterScreen;
+            fmShow.Font = this.Font;
+            fmShow.Icon = this.Icon;
+            fmShow.Text = "All Data";
+            fmShow.Size = this.Size;
+            txtShow.Multiline = true;
+            txtShow.Dock = DockStyle.Fill;
+            fmShow.Controls.Add(txtShow);
+
+            try
+            {
+                StreamReader sw = new StreamReader("Data.txt");
+                string data = sw.ReadToEnd();
+                sw.Close();
+                txtShow.Text = data;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            fmShow.ShowDialog();
+        }
+    }
+}
